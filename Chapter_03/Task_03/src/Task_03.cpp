93774cc0c54ec5f6724871d40436293456bfec04
Filip Andonov <filip_andonow@abv.bg>
2020-11-20 13:35:04 +0200

    #12

---
+++ Chapter_03/Task_03/src/Task_03.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+
+int main() 
+{
+	int value = 0;
+	std::string input;
+
+	while (true) 
+	{
+		std::cin >> input;
+
+		if (input.compare("programist") == 0)
+		{
+			value = 1;
+		}
+		else if (input == "Toma")
+		{
+			value = 2;
+		}
+		else if (input == "End")
+		{
+			break;
+		}
+
+		switch (value)
+		{
+		case 1:
+			std::cout << "na fortran" << "\n";
+			break;
+
+		case 2:
+			std::cout << "J 007" << "\n";
+			break;
+
+		default:
+			break;
+		}
+
+		value = 0;
+
+	}
+
+	return 0;
+}
