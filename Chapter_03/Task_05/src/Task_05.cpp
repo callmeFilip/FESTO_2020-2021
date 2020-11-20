b051ad462d49b9f922f7a1ecda67486f233f1f8c
Filip Andonov <filip_andonow@abv.bg>
2020-11-20 14:21:40 +0200

    #14

---
+++ Chapter_03/Task_05/src/Task_05.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+
+    int X = 2;
+    int Y = 4;
+    int Z = 3;
+    int A = 0;
+    
+    A = X + Y - 2 / 2 + Z;
+
+    std::cout << A << "\n";
+
+    A = X + (Y - 2) / (2 + Z);
+
+    std::cout << A << "\n";
+
+}
