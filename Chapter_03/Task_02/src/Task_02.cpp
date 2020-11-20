96aff99e8fd68bfb986122753e52b4e3bb19f580
Filip Andonov <filip_andonow@abv.bg>
2020-11-20 12:55:21 +0200

    #11

---
+++ Chapter_03/Task_02/src/Task_02.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main()
+{
+	bool flag = true;
+	for (int i = 2; i < 200; i++)
+	{
+
+		for (int j = 2; j <= sqrt(i) ; j++) 
+		{
+			if (i % j == 0) 
+			{
+				flag = false;
+			}
+		}
+
+		if (flag) 
+		{
+			std::cout << i << "\n";
+		}
+		else
+		{
+			flag = true;
+		}
+
+		
+	}
+}
