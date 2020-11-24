44a087aa31b9befa42b189bc79c2fea583d486f8
Filip Andonov <filip_andonow@abv.bg>
2020-11-24 11:27:54 +0200

    #25

---
+++ Chapter_03/Task_16/src/Task_16.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+enum Colors 
+{
+    red, blue, green, yellow
+};
+
+int main()
+{
+
+    Colors colorPallet;
+
+    for (int i = 0; i < sizeof(colorPallet);i++) 
+    {
+        std::cout << i << std::endl;
+    }
+
+}
