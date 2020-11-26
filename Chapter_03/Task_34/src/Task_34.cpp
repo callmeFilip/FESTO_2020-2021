4b97b332bb6d6efd6f745eb2232c53b9f32c1160
Filip Andonov <filip_andonow@abv.bg>
2020-11-26 15:55:10 +0200

    #43

--- Chapter_03/Task_34/src/Task_34.cpp
+++ Chapter_03/Task_34/src/Task_34.cpp
@@ -5,37 +5,47 @@
 using namespace std;
 
 // A macro to define dummy functions:
-#define DF(N) std::string N() { \
-cout << "function " #N " called..." << endl; \
-return #N;\
-}
+#define DF(N)                                        \
+    std::string N()                                  \
+    {                                                \
+        cout << "function " #N " called..." << endl; \
+        return #N;                                   \
+    }
 
-DF(a); DF(b); DF(c); DF(d); DF(e); DF(f); DF(g);
+DF(a);
+DF(b);
+DF(c);
+DF(d);
+DF(e);
+DF(f);
+DF(g);
 
-std::string (*func_table[])() = { a, b, c, d, e, f, g };
+std::string (*func_table[])() = {a, b, c, d, e, f, g};
 
 int main()
 {
 
-    while (1) 
+    while (1)
     {
         cout << "press a key from 'a' to 'g' "
-            "or q to quit" << endl;
+                "or q to quit"
+             << endl;
         char c, cr;
-        cin.get(c); cin.get(cr); // second one for CR
-        if (c == 'q') 
+        cin.get(c);
+        cin.get(cr); // second one for CR
+        if (c == 'q')
         {
             break; // ... out of while(1)
         }
 
-        if (c < 'a' || c > 'g') 
+        if (c < 'a' || c > 'g')
         {
             continue;
         }
 
         std::cout << "::" << (*func_table[c - 'a'])() << "::" << std::endl;
-
     }
+
     return 0;
 
-} ///:~ 
+} ///:~
