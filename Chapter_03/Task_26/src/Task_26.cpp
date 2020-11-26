21a695f928a0de5f01a17fac80c582a1bc80115e
Filip Andonov <filip_andonow@abv.bg>
2020-11-26 10:58:48 +0200

    #35

---
+++ Chapter_03/Task_26/src/Task_26.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+#define SIZE 5 // size of array
+#define VALUE 0 // value to be set for each element
+
+void setArr(void* n_voidPtr, int n_NumberOfBytes, int n_Value) 
+{
+	int* ptr_intArr;
+
+	ptr_intArr = static_cast<int*> (n_voidPtr);
+
+	for (int i = 0 ; i < n_NumberOfBytes ; i++) 
+	{
+		ptr_intArr[i] = n_Value;
+	}
+}
+
+int main()
+{
+
+	int intArr[5];
+
+	void* voidPtr;
+
+	voidPtr = static_cast<void*> (intArr);
+
+	setArr(voidPtr, SIZE, VALUE);
+
+	for (int i : intArr) 
+	{
+		std::cout << i << std::endl;
+	}
+
+	return 0;
+	
+}
