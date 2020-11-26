f750f24a030f62f9a97bc52963a356907b65295a
Filip Andonov <filip_andonow@abv.bg>
2020-11-26 09:33:25 +0200

    #31

---
+++ Chapter_03/Task_22/src/Task_22.cpp
@@ -0,0 +1,21 @@
+//: C03:ArgsToInts.cpp
+// Converting command-line arguments to ints
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+    for (int i = 1; i < argc; i++)
+    {
+        cout << atoi(argv[i]) << endl;
+    }
+
+
+    for (int i = 1; i < argc; i++)
+    {
+        cout << atof(argv[i]) << endl;
+    }
+
+    return 0;
+} ///:~ 
