d00f5d288d6f25d40b4f60366de7837acc21f822
Filip Andonov <filip_andonow@abv.bg>
2020-12-04 10:18:29 +0200

    #42

--- Chapter_03/Task_33/src/Task_33.cpp
+++ Chapter_03/Task_33/src/Task_33.cpp
@@ -4,29 +4,29 @@ typedef float (*ptr_floatFunc) (char);
 
 float floatFunction(char n_symbol) 
 {
-	std::cout << 1.0f << std::endl;
+    std::cout << 1.0f << std::endl;
 
-	return 1.0f;
+    return 1.0f;
 }
 
 ptr_floatFunc intFunction(int n_number)
 {
-	float (*ptr_floatFunc) (char);
+    float (*ptr_floatFunc) (char);
 
-	ptr_floatFunc = floatFunction;
+    ptr_floatFunc = floatFunction;
 
-	return ptr_floatFunc;
+    return ptr_floatFunc;
 }
 
 int main()
 {
 
-	ptr_floatFunc(*funcPtr) (int);
+    ptr_floatFunc(*funcPtr) (int);
 
-	funcPtr = intFunction;
+    funcPtr = intFunction;
 
-	std::cout << (*funcPtr(2)) ('d')<< std::endl;
+    std::cout << (*funcPtr(2)) ('d')<< std::endl;
 
-	return 0;
+    return 0;
 
 }
