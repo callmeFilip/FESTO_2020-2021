f37b6ca538c2e813872fcdb6fd42ac5a3dfd5e72
Filip Andonov <filip_andonow@abv.bg>
2020-12-04 10:11:20 +0200

    #24

--- Chapter_03/Task_15/src/Person.cpp
+++ Chapter_03/Task_15/src/Person.cpp
@@ -1,7 +1,7 @@
 #include "../inc/Person.h"
 void Person::print(void)
 {
-	std::cout << "\n\nPerson named: " << m_firstName << " " << m_lastName
-		<< " is " << m_age << " years old!\n\n";
+    std::cout << "\n\nPerson named: " << m_firstName << " " << m_lastName
+        << " is " << m_age << " years old!\n\n";
 
 }
