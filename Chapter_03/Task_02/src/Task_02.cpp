d97e76f6df6ec3f6fb4ceab031d6f1b7bcc1a670
Filip Andonov <filip_andonow@abv.bg>
2020-12-04 10:07:24 +0200

    #11

--- Chapter_03/Task_02/src/Task_02.cpp
+++ Chapter_03/Task_02/src/Task_02.cpp
@@ -3,28 +3,28 @@
 
 int main()
 {
-	bool isPrime;
-	const unsigned int numberRange = 200;
+    bool isPrime;
+    const unsigned int numberRange = 200;
 
-	for (int i = 2; i < numberRange; i++)
-	{
-		isPrime = true;
-		unsigned int iterator_end = sqrt(i);
+    for (int i = 2; i < numberRange; i++)
+    {
+        isPrime = true;
+        unsigned int iterator_end = sqrt(i);
 
-		for (int j = 2; j <= iterator_end; j++)
-		{
-			if (i % j == 0)
-			{
-				isPrime = false;
-				break;
-			}
-		}
+        for (int j = 2; j <= iterator_end; j++)
+        {
+            if (i % j == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
 
-		if (isPrime)
-		{
-			std::cout << i << std::endl;
-		}
-	}
+        if (isPrime)
+        {
+            std::cout << i << std::endl;
+        }
+    }
 
-	return 0;
+    return 0;
 }
