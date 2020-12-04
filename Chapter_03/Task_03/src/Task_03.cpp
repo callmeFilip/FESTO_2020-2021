3ff60b4c030a245882a50de230525905d1c90367
Filip Andonov <filip_andonow@abv.bg>
2020-12-04 10:07:51 +0200

    #12

--- Chapter_03/Task_03/src/Task_03.cpp
+++ Chapter_03/Task_03/src/Task_03.cpp
@@ -3,43 +3,43 @@
 
 int main() 
 {
-	int value = 0;
-	std::string input;
+    int value = 0;
+    std::string input;
 
-	while (true) 
-	{
-		std::cin >> input;
+    while (true) 
+    {
+        std::cin >> input;
 
-		if (input.compare("programist") == 0)
-		{
-			value = 1;
-		}
-		else if (input == "Toma")
-		{
-			value = 2;
-		}
-		else if (input == "End")
-		{
-			break;
-		}
+        if (input.compare("programist") == 0)
+        {
+            value = 1;
+        }
+        else if (input == "Toma")
+        {
+            value = 2;
+        }
+        else if (input == "End")
+        {
+            break;
+        }
 
-		switch (value)
-		{
-		case 1:
-			std::cout << "na fortran" << "\n";
-			break;
+        switch (value)
+        {
+        case 1:
+            std::cout << "na fortran" << "\n";
+            break;
 
-		case 2:
-			std::cout << "J 007" << "\n";
-			break;
+        case 2:
+            std::cout << "J 007" << "\n";
+            break;
 
-		default:
-			break;
-		}
+        default:
+            break;
+        }
 
-		value = 0;
+        value = 0;
 
-	}
+    }
 
-	return 0;
+    return 0;
 }
