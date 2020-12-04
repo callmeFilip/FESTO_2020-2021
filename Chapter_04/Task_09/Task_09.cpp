3e70f0327e6e6e1db6ea0aefd1421da6030db02c
Filip Andonov <filip_andonow@abv.bg>
2020-12-04 15:43:17 +0200

    #52

---
+++ Chapter_04/Task_09/Task_09.cpp
@@ -0,0 +1,11 @@
+#include "function.h"
+
+void f(float n_number)
+{
+}
+
+int main()
+{
+    f(5.0f);
+    return 0;
+}
---
+++ Chapter_04/Task_09/function.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "function.h"
+
+void f(int n_number)
+{
+    printf("%d/n", n_number);
+}
