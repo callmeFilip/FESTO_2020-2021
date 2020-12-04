f9038317a2d46c650925103c65592579d8c6894d
Filip Andonov <filip_andonow@abv.bg>
2020-12-04 10:16:29 +0200

    #36

--- Chapter_03/Task_27/src/Task_27.cpp
+++ Chapter_03/Task_27/src/Task_27.cpp
@@ -2,21 +2,21 @@
 #define ARRAYSIZE 5
 int main()
 {
-	const double CD_Arr[ARRAYSIZE] { 0.0 };
-	volatile double VD_Arr[ARRAYSIZE] { 0.0 };
+    const double CD_Arr[ARRAYSIZE] { 0.0 };
+    volatile double VD_Arr[ARRAYSIZE] { 0.0 };
 
-	double* temp_ptr = nullptr;
+    double* temp_ptr = nullptr;
 
-	for (int i = 0; i < ARRAYSIZE; i++) 
-	{
-		temp_ptr = const_cast <double*> (&CD_Arr[i]);
-		*temp_ptr = 1;
+    for (int i = 0; i < ARRAYSIZE; i++) 
+    {
+        temp_ptr = const_cast <double*> (&CD_Arr[i]);
+        *temp_ptr = 1;
 
-		temp_ptr = const_cast <double*> (&VD_Arr[i]);
-		*temp_ptr = 1;
-	
-		std::cout << CD_Arr[i] << "\t" << VD_Arr[i] << "\n";
-	}
+        temp_ptr = const_cast <double*> (&VD_Arr[i]);
+        *temp_ptr = 1;
+    
+        std::cout << CD_Arr[i] << "\t" << VD_Arr[i] << "\n";
+    }
 
-	return 0;
+    return 0;
 }
