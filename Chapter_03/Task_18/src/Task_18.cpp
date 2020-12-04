99cd8d68297079bdf2a6036f654b7de2adcc3f92
Filip Andonov <filip_andonow@abv.bg>
2020-12-04 10:12:54 +0200

    #27

--- Chapter_03/Task_18/src/Task_18.cpp
+++ Chapter_03/Task_18/src/Task_18.cpp
@@ -2,45 +2,45 @@
 
 void printArr(int* n_firstArr, const int n_firstArr_size)
 {
-	for (int i = 0; i < n_firstArr_size; i++)
-	{
-		std::cout << n_firstArr[i] << std::endl;
-	}
+    for (int i = 0; i < n_firstArr_size; i++)
+    {
+        std::cout << n_firstArr[i] << std::endl;
+    }
 
-	std::cout << "\n\n\n\n" <<std::endl;
+    std::cout << "\n\n\n\n" <<std::endl;
 
 }
 
 int main()
 {
-	const int firstArr_size = 10;
-	const int secondArr_size = 5;
+    const int firstArr_size = 10;
+    const int secondArr_size = 5;
 
-	int firstArr[firstArr_size];
-	char ch = 'a';
-	int secondArr[secondArr_size];
+    int firstArr[firstArr_size];
+    char ch = 'a';
+    int secondArr[secondArr_size];
 
-	for (int i = 0; i < firstArr_size; i++)
-	{
-		firstArr[i] = 0;
+    for (int i = 0; i < firstArr_size; i++)
+    {
+        firstArr[i] = 0;
 
-		if (i < secondArr_size)
-		{
-			secondArr[i] = 0;
-		}
+        if (i < secondArr_size)
+        {
+            secondArr[i] = 0;
+        }
 
-	}
+    }
 
-	std::cout <<(long)&ch << "\n" << (long)&firstArr << "\n" << (long)&secondArr << "\n";
+    std::cout <<(long)&ch << "\n" << (long)&firstArr << "\n" << (long)&secondArr << "\n";
 
-	printArr(firstArr, firstArr_size);
-	printArr(secondArr, secondArr_size);
+    printArr(firstArr, firstArr_size);
+    printArr(secondArr, secondArr_size);
 
-	secondArr[12] = 1; // nqmam si i na ideq zashto secondArr e zapisan predi firstArr v pametta na moqt PC
+    secondArr[12] = 1; // nqmam si i na ideq zashto secondArr e zapisan predi firstArr v pametta na moqt PC
 
-	printArr(firstArr, firstArr_size);
-	printArr(secondArr, secondArr_size);
+    printArr(firstArr, firstArr_size);
+    printArr(secondArr, secondArr_size);
 
 
-	return 0;
+    return 0;
 }
