3e9c618e9a005e8d0905c53a0a1ede6534933da3
Filip Andonov <filip_andonow@abv.bg>
2020-12-04 10:17:56 +0200

    #41

--- Chapter_03/Task_32/src/Task_32.cpp
+++ Chapter_03/Task_32/src/Task_32.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 int function(double n_number) 
 {
-	return 12;
+    return 12;
 }
 
 int main()
 {
-	int (*ptr_func) (double) = function;
+    int (*ptr_func) (double) = function;
 
-	std::cout << (*ptr_func)(2.4);
+    std::cout << (*ptr_func)(2.4);
 
-	return 0;
+    return 0;
 
 }
