6fea5cc0dfa1c4c819411edae9664f3a78fc3667
Filip Andonov <filip_andonow@abv.bg>
2020-12-04 10:08:39 +0200

    #16

--- Chapter_03/Task_07/src/Task_07.cpp
+++ Chapter_03/Task_07/src/Task_07.cpp
@@ -1,28 +1,28 @@
 #include <iostream>
 void ptrPassString(std::string* n_string)
 {
-	*n_string = "Modification 1";
+    *n_string = "Modification 1";
 }
 
 void refPassString(std::string& n_string)
 {
-	n_string = "HOUSE WORK (Froglicka Remix) e stranna pesen";
+    n_string = "HOUSE WORK (Froglicka Remix) e stranna pesen";
 }
 
 
 int main()
 {
-	std::string str = "Zabravih makaronite na kotlona za 45 minuti i te stanaha po-vkusno ot obiknoveno?!";
+    std::string str = "Zabravih makaronite na kotlona za 45 minuti i te stanaha po-vkusno ot obiknoveno?!";
 
-	std::cout << str << std::endl;
+    std::cout << str << std::endl;
 
-	ptrPassString(&str);
-	std::cout << str << std::endl;
+    ptrPassString(&str);
+    std::cout << str << std::endl;
 
 
-	refPassString(str);
-	std::cout << str << std::endl;
+    refPassString(str);
+    std::cout << str << std::endl;
 
-	return 0;
+    return 0;
 
 }
