3c6c2990aa8a652d86e02fd6eade0f4080c89adc
Filip Andonov <filip_andonow@abv.bg>
2020-12-10 10:20:58 +0200

    #27

--- Chapter_03/Task_18/src/Task_18.cpp
+++ Chapter_03/Task_18/src/Task_18.cpp
@@ -1,19 +1,34 @@
 #include <iostream>
-
-void printArr(int* n_firstArr, const int n_firstArr_size)
+#define PRINTAND(EXP) #EXP << ": " << (long)&EXP << std::endl
+void printArr(int *n_firstArr, const int n_firstArr_size)
 {
     for (int i = 0; i < n_firstArr_size; i++)
     {
         std::cout << n_firstArr[i] << std::endl;
     }
 
-    std::cout << "\n\n\n\n" <<std::endl;
-
+    std::cout << "\n\n\n\n"
+              << std::endl;
 }
+
+int GLOBAL_YEARS = 2020;
+int GLOBAL_MONTHS = 12;
+int GLOBAL_DAYS = 10;
 
 int main()
 {
-    const int firstArr_size = 10;
+    int height = 100;
+    int width = 500;
+    int galonsPerMile = 200;
+
+    std::cout << PRINTAND(height)
+              << PRINTAND(width)
+              << PRINTAND(galonsPerMile)
+              << PRINTAND(GLOBAL_YEARS)
+              << PRINTAND(GLOBAL_MONTHS)
+              << PRINTAND(GLOBAL_DAYS);
+
+    /*    const int firstArr_size = 10;
     const int secondArr_size = 5;
 
     int firstArr[firstArr_size];
@@ -40,7 +55,7 @@ int main()
 
     printArr(firstArr, firstArr_size);
     printArr(secondArr, secondArr_size);
-
+*/
 
     return 0;
 }
