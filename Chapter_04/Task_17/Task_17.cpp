e59909905e02473a88bd2d8050703598ad770e46
Filip Andonov <filip_andonow@abv.bg>
2020-12-10 14:38:07 +0200

    #59

---
+++ Chapter_04/Task_17/Task_17.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int *smallFish = new int(0);
+    long *longFish = new long(0);
+    char *name = new char[100];
+    float *measurements = new float[100];
+
+    cout << (long)smallFish << endl
+         << (long)longFish << endl
+         << (long)name << endl
+         << (long)measurements << endl
+         << endl;
+
+    delete smallFish;
+    delete longFish;
+    delete[] name;
+    delete[] measurements;
+
+    return 0;
+}
