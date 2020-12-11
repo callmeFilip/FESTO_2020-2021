68efadaa22cf46edb9b6c7ef28bb25492ba4faf5
Filip Andonov <filip_andonow@abv.bg>
2020-12-11 15:12:11 +0200

    Modify Exercise 22 so that you create a struct that encapsulates the Stack of Stashes. The user should only add and get lines via member functions, but under the covers the struct happens to use a Stack of Stashes.

---
+++ Chapter_04/Task_23/StashedStack.cpp
@@ -0,0 +1,70 @@
+#include "StashedStack.h"
+#include <fstream>
+#include <vector>
+StashedStack::StashedStack()
+{
+    m_stack.initialize();
+}
+
+StashedStack::~StashedStack()
+{
+    m_stack.cleanup();
+}
+
+void StashedStack::push(Stash *element)
+{
+    m_stack.push(element);
+}
+
+Stash *StashedStack::pop()
+{
+    Stash *result;
+
+    result = (Stash *)m_stack.pop();
+
+    return result;
+}
+
+void StashedStack::fillFromFile(std::string path)
+{
+    Stash *fiveRowsStash;
+
+    std::ifstream write(path);
+    std::string bufferString;
+    std::string fiveRows = "";
+    int rowCounter = 0;
+
+    std::vector<std::string> reverser;
+
+    //getting lines from test.txt and saving them into stash elements
+    while (getline(write, bufferString))
+    {
+        rowCounter++;
+        fiveRows += bufferString + "\n";
+
+        //every 5th row
+        if (rowCounter == 5)
+        {
+            //adds the fiveRows to a vector
+            reverser.push_back(fiveRows);
+
+            //zeroing
+            rowCounter = 0;
+            fiveRows = "";
+        }
+    }
+
+    while (reverser.size() != 0)
+    {
+        //size is produced from the size of the content in vector's last element
+        fiveRowsStash = new Stash(reverser[reverser.size() - 1].size());
+
+        //adding five rows from the text file to a stash
+        fiveRowsStash->add(reverser[reverser.size() - 1].c_str());
+
+        m_stack.push(fiveRowsStash); // filling the storage with the stash created
+
+        //removing last element from vector
+        reverser.pop_back();
+    }
+}
---
+++ Chapter_04/Task_23/StashedStack.h
@@ -0,0 +1,21 @@
+#ifndef STASHEDSTACK_H
+#define STASHEDSTACK_H
+#include "Header.h"
+#include "string"
+
+struct StashedStack
+{
+    //members
+    Stack m_stack;
+
+    //constructors and destructors
+    StashedStack();
+    ~StashedStack();
+
+    //methods
+    void push(Stash *element);
+    Stash *pop(void);
+    void fillFromFile(std::string path);
+};
+
+#endif
