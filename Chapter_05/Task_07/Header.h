e00e4e3a7f00155c3cac26297cf202191ca496b0
Filip Andonov <filip_andonow@abv.bg>
2020-12-18 12:38:15 +0200

    #73

---
+++ Chapter_05/Task_07/Header.h
@@ -0,0 +1,48 @@
+#ifndef HEADER_H
+#define HEADER_H
+#include <iostream>
+
+class Hen
+{
+public:
+    class Nest
+    {
+    public:
+        class Egg
+        {
+            //members
+            int size;
+
+        public:
+            //constructors
+            Egg();
+
+            //methods
+            void display();
+
+            //friends
+            friend Nest;
+        };
+
+    private:
+        //members
+        int rooms;
+        Egg egg;
+
+    public:
+        //constructors
+        Nest();
+
+        //methods
+        void display();
+
+        //friends
+        friend Hen;
+    };
+
+    Nest nest;
+    //methods
+    void display();
+};
+
+#endif
---
+++ Chapter_05/Task_07/Task_07.cpp
@@ -0,0 +1,42 @@
+#include "Header.h"
+#include <iostream>
+
+//hen initialization
+void Hen::display()
+{
+    std::cout << "Rooms in nest: " << nest.rooms << std::endl;
+}
+
+void Hen::Nest::display()
+{
+    std::cout << "Size of egg: " << egg.size << std::endl;
+}
+
+Hen::Nest::Nest()
+{
+    rooms = 1;
+}
+
+void Hen::Nest::Egg::display()
+{
+    std::cout << "Egg" << std::endl;
+}
+
+Hen::Nest::Egg::Egg()
+{
+    size = 2;
+}
+
+int main()
+{
+
+    Hen hen;
+    Hen::Nest nest;
+    Hen::Nest::Egg egg;
+
+    hen.display();
+    nest.display();
+    egg.display();
+
+    return 0;
+}
