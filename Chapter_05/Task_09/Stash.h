326c8e0b883f561bb3894bf1fa291653a8451397
Filip Andonov <filip_andonow@abv.bg>
2020-12-18 14:03:28 +0200

    #76

--- Chapter_05/Task_09/Stash.h
+++ Chapter_05/Task_09/Stash.h
@@ -1,17 +1,19 @@
 //: C04:CppLib.h
 // C-like library converted to C++
-struct Stash
+class Stash
 {
     int size;     // Size of each space
     int quantity; // Number of storage spaces
     int next;     // Next empty space
     // Dynamically allocated array of bytes:
     unsigned char *storage;
+    void inflate(int increase);
+
+public:
     // Functions!
     void initialize(int size);
     void cleanup();
     int add(const void *element);
     void *fetch(int index);
     int count();
-    void inflate(int increase);
 }; ///:~
