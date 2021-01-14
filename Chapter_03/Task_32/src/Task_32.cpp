18074632ba3534ed1cf4ea225a0b8d855b5f7bff
Filip Andonov <filip_andonow@abv.bg>
2021-01-14 10:39:42 +0200

    #103

--- Chapter_03/Task_32/src/Task_32.cpp
+++ Chapter_03/Task_32/src/Task_32.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
-int function(double n_number) 
+int function(double n_number)
 {
     return 12;
 }
 
 int main()
 {
-    int (*ptr_func) (double) = function;
+    int (*ptr_func)(double) = function;
 
     std::cout << (*ptr_func)(2.4);
 
     return 0;
-
 }
