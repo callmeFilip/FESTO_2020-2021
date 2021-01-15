8c8c8f888fd1a476fd481cc93b58548278063685
Filip Andonov <filip_andonow@abv.bg>
2021-01-15 14:50:37 +0200

    #126

---
+++ Chapter_08/Task_23/Task_23.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "Header.h"
+
+void Animal::makeSound() const
+{
+    std::cout << "PRRRRR" << std::endl;
+}
+
+void Animal::setAge(int age) {}
+
+int main()
+{
+    Animal dog;
+
+    dog.makeSound();
+    dog.setAge(5);
+
+    const Animal snake;
+
+    snake.makeSound();
+    //! snake.setAge(5); //passing 'const Animal' as 'this' argument discards qualifiers [-fpermissive]
+
+    return 0;
+}
