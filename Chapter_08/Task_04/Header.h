f03966d304891ed6483fbd009d80404ce57814fa
Filip Andonov <filip_andonow@abv.bg>
2021-01-15 09:57:05 +0200

    #108

---
+++ Chapter_08/Task_04/Header.h
@@ -0,0 +1,6 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+const int PERIMETER = 5;
+
+#endif
---
+++ Chapter_08/Task_04/Task_04.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "Header.h"
+
+int main()
+{
+    printf("%d\n", PERIMETER);
+    return 0;
+}
+
+//compiles whitout any errors nor warnings in C and C++
