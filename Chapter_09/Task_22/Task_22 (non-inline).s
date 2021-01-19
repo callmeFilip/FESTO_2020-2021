f24735edff0e663b3753431296f1b287baa7777d
Filip Andonov <filip_andonow@abv.bg>
2021-01-19 15:23:03 +0200

    #154

---
+++ Chapter_09/Task_22/Task_22 (non-inline).s
@@ -0,0 +1,44 @@
+	.file	"Task_22.cpp"
+	.text
+	.globl	__Z8functionv
+	.def	__Z8functionv;	.scl	2;	.type	32;	.endef
+__Z8functionv:
+LFB0:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$16, %esp
+	movl	$5, -4(%ebp)
+	addl	$1, -4(%ebp)
+	movl	-4(%ebp), %eax
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE0:
+	.def	___main;	.scl	2;	.type	32;	.endef
+	.globl	_main
+	.def	_main;	.scl	2;	.type	32;	.endef
+_main:
+LFB1:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	andl	$-16, %esp
+	call	___main
+	call	__Z8functionv
+	movl	$0, %eax
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE1:
+	.ident	"GCC: (MinGW.org GCC Build-2) 9.2.0"
---
+++ Chapter_09/Task_22/Task_22.cpp
@@ -0,0 +1,15 @@
+int function()
+{
+    int a = 5;
+    a++;
+    return a;
+}
+
+int main(int argc, char const *argv[])
+{
+    function();
+
+    return 0;
+}
+
+// Why is the function called with inline modificator?
