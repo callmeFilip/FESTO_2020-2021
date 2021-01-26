069241b5bb1a8decbc8069c107b339bfa29411d9
Filip Andonov <filip_andonow@abv.bg>
2021-01-26 15:49:15 +0200

    #162

---
+++ Chapter_10/Task_08/StaticDestructors.cpp
@@ -0,0 +1,53 @@
+//: C10:StaticDestructors.cpp
+// Static object destructors
+#include <fstream>
+
+using namespace std;
+
+ofstream out("statdest.out"); // Trace file
+
+class Obj
+{
+    char c; // Identifier
+public:
+    Obj(char cc) : c(cc)
+    {
+        out << "Obj::Obj() for " << c << endl;
+    }
+    ~Obj()
+    {
+        out << "Obj::~Obj() for " << c << endl;
+    }
+};
+
+Obj a('a'); // Global (static storage)
+// Constructor & destructor always called
+void f()
+{
+    static Obj b('b');
+}
+
+void g()
+{
+    static Obj c('c');
+}
+
+int main()
+{
+    out << "inside main()" << endl;
+    g();
+    f(); // Calls static constructor for b
+
+    out << "leaving main()" << endl;
+} ///:~
+
+/**
+// ORDER:
+// f() then g();
+// a, b, c / c, b, a
+// 
+// g() then f()
+// a, c, b / b, c, a
+// ANSWER:
+// Yes, the order is right.
+*/
