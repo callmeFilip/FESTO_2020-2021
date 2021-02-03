69826e42d09d3f872bb01e7063947454e636fa07
Filip Andonov <filip_andonow@abv.bg>
2021-02-03 16:29:15 +0200

    #51

--- Chapter_04/Task_08/Task_08.cpp
+++ Chapter_04/Task_08/Task_08.cpp
@@ -1,9 +1,20 @@
 #include "Stack.h"
+#include <iostream>
 int main()
 {
     Stack doubleStack;
 
     doubleStack.initialize();
+
+    int a = 10;
+    doubleStack.push(&a);
+    std::cout << (*(int *)doubleStack.peek()) << std::endl;
+
+    int b = 13;
+    doubleStack.push(&b);
+
+    std::cout << (*(int *)doubleStack.pop()) << std::endl;
+    std::cout << (*(int *)doubleStack.pop()) << std::endl;
 
     return 0;
 }
