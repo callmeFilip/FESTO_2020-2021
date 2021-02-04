40d8b035917e7a2bc2f349405f9738ed187acb93
Filip Andonov <filip_andonow@abv.bg>
2021-02-04 10:08:09 +0200

    #191

---
+++ Chapter_11/Task_01/Task_01.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+typedef struct Bird
+{
+    int fly;
+    int age;
+} Bird;
+
+typedef struct Rock
+{
+    double size;
+} Rock;
+
+int main()
+{
+    Bird *bird;
+    Rock *stone;
+    void *vPtr = &bird;
+
+    bird = vPtr; // invalid conversion from 'void*' to 'Bird*' in C++
+                 // works fine, when compiled in C
+
+    printf("End of main..");
+
+    return 0;
+}
