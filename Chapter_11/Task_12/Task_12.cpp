792c344e2f3f0fd4da94400a52c0d84953768460
Filip Andonov <filip_andonow@abv.bg>
2021-02-04 13:31:31 +0200

    #202

---
+++ Chapter_11/Task_12/Task_12.cpp
@@ -0,0 +1,12 @@
+double function()
+{
+    double result = 12.4;
+
+    return result;
+}
+
+int main()
+{
+    function();
+    return 0;
+}
---
+++ Chapter_11/Task_12/Task_12.s
@@ -0,0 +1,50 @@
+	.file	"Task_12.cpp"
+	.text
+	.globl	__Z8functionv
+	.def	__Z8functionv;	.scl	2;	.type	32;	.endef
+__Z8functionv:
+LFB0:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$16, %esp
+	fldl	LC0
+	fstpl	-8(%ebp)
+	fldl	-8(%ebp)
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE0:
+	.def	___main;	.scl	2;	.type	32;	.endef
+	.globl	_main
+	.def	_main;	.scl	2;	.type	32;	.endef
+_main:
+LFB1:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	andl	$-16, %esp
+	call	___main
+	call	__Z8functionv
+	fstp	%st(0)
+	movl	$0, %eax
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE1:
+	.section .rdata,"dr"
+	.align 8
+LC0:
+	.long	-858993459
+	.long	1076415692
+	.ident	"GCC: (MinGW.org GCC Build-2) 9.2.0"
