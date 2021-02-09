73dc907eca993b342963b1769d6e9d87f7caf9fd
Filip Andonov <filip_andonow@abv.bg>
2021-02-09 12:34:13 +0200

    #190

---
+++ Chapter_04/Task_16/Stash.h
@@ -0,0 +1,26 @@
+#ifndef STASH_H
+#define STASH_H
+#include <vector>
+
+struct Stash
+{
+    /* data */
+    int m_size;     //size of each element
+    int m_quantity; // number of elements
+    int m_next;     //next empty space
+
+    std::vector<char> m_storage;
+
+    //constructors and destructors
+
+    Stash();    //default sets the type to char-size
+    Stash(int); //sets the type to the size given
+    ~Stash();   //cleanup
+
+    //methods
+    bool add(const void *element);
+    void *at(int index);
+    int length();
+};
+
+#endif
