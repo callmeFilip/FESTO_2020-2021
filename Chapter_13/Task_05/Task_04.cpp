7e4d932ae9512676a5a5252a84a5835350093bdc
Filip Andonov <filip_andonow@abv.bg>
2021-02-13 12:46:18 +0200

    #255

---
+++ Chapter_13/Task_05/Task_04.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include "Counted.h" // + iostream
+
+int main()
+{
+    const int COUNTEDELEMENTS = 10;
+    std::vector<Counted *> storage;
+
+    for (int i = 0; i < COUNTEDELEMENTS; i++)
+    {
+        storage.push_back(new Counted());
+    }
+
+    for (int i = 0; i < COUNTEDELEMENTS; i++)
+    {
+        storage[i]->print();
+    }
+
+    for (int i = COUNTEDELEMENTS - 1; i >= 0; i--)
+    {
+        delete storage[i];
+        storage[i] = 0;
+
+        storage.pop_back();
+    }
+
+    storage.shrink_to_fit();
+
+    return 0;
+}
