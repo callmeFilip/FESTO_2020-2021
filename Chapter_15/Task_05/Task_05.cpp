e785fe1eab8ca22f68dc2629dd91394453c638af
Filip Andonov <filip_andonow@abv.bg>
2021-02-28 20:42:14 +0200

    #299

---
+++ Chapter_15/Task_05/Task_05.cpp
@@ -0,0 +1,55 @@
+//: C15:Instrument3.cpp
+// Late binding with the virtual keyword
+#include <iostream>
+using namespace std;
+enum note
+{
+    middleC,
+    Csharp,
+    Cflat
+}; // Etc.
+
+class Instrument
+{
+public:
+    virtual void play(note) const
+    {
+        cout << "Instrument::play" << endl;
+    }
+
+    virtual void prepare() const
+    {
+        std::cout << "Instrument::prepare" << std::endl;
+    }
+};
+
+// Wind objects are Instruments
+// because they have the same interface:
+class Wind : public Instrument
+{
+public:
+    // Override interface function:
+    void play(note) const
+    {
+        cout << "Wind::play" << endl;
+    }
+    void prepare() const
+    {
+        std::cout << "Wind::prepare" << std::endl;
+    }
+};
+
+void tune(Instrument &i)
+{
+    // ...
+    i.play(middleC);
+    i.prepare();
+}
+
+int main()
+{
+    Wind flute;
+    tune(flute); // Upcasting
+
+    return 0;
+} ///:~
