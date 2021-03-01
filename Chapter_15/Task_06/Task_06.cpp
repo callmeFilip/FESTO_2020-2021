39ff5eaf8a7b78dbbd603308384d78b630974b13
Filip Andonov <filip_andonow@abv.bg>
2021-03-01 11:05:59 +0200

    #300

---
+++ Chapter_15/Task_06/Task_06.cpp
@@ -0,0 +1,20 @@
+#include "Derivatives.h"
+
+int main()
+{
+    const size_t ARRAYSIZE = 4;
+    Rodent *arr[] = {new GuineaPig, new Gerbil, new Hamster, new Mouse};
+
+    for (size_t i = 0; i < ARRAYSIZE; i++)
+    {
+        arr[i]->Rodent::eat();
+    }
+
+    for (size_t i = 0; i < ARRAYSIZE; i++)
+    {
+        delete arr[i]; // i am not sure if the right object types will be deleted with delete[]
+        arr[i] = 0;
+    }
+
+    return 0;
+}
