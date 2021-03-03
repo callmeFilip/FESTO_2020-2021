5471b02d6d0f3e8341599ad8319f65682a425b2f
Filip Andonov <filip_andonow@abv.bg>
2021-03-03 11:26:25 +0200

    #301

--- Chapter_15/Task_07/Task_07.cpp
+++ Chapter_15/Task_07/Task_07.cpp
@@ -3,22 +3,22 @@
 
 int main()
 {
-    const size_t ARRAYSIZE = 4;
-
     std::vector<Rodent *> arr;
     arr.push_back(new GuineaPig);
     arr.push_back(new Gerbil);
     arr.push_back(new Hamster);
     arr.push_back(new Mouse);
 
-    for (size_t i = 0; i < ARRAYSIZE; i++)
+    const size_t VSIZE = arr.size();
+
+    for (size_t i = 0; i < VSIZE; i++)
     {
         arr[i]->Rodent::eat();
         arr[i]->Rodent::gnaw();
         std::cout << std::endl;
     }
 
-    for (size_t i = 0; i < ARRAYSIZE; i++)
+    for (size_t i = 0; i < VSIZE; i++)
     {
         delete arr[i]; // i am not sure if the right object types will be deleted with delete[]
         arr[i] = 0;
