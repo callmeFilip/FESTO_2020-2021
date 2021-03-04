9c44281391a6ae91a98ff70499a468c7cc4f73fc
Filip Andonov <filip_andonow@abv.bg>
2021-03-04 13:19:37 +0200

    #307

---
+++ Chapter_15/Task_13/Task_13.cpp
@@ -0,0 +1,34 @@
+//: C15:Early.cpp
+// Early binding & virtual functions
+#include <iostream>
+#include <string>
+using namespace std;
+class Pet
+{
+public:
+    virtual string speak() const = 0;
+};
+
+inline string Pet::speak() const { return ""; }
+
+class Dog : public Pet
+{
+public:
+    string speak() const { return "Bark!"; }
+};
+
+int main()
+{
+    Dog ralph;
+    Pet *p1 = &ralph;
+    Pet &p2 = ralph;
+    //! Pet p3; // abstract class
+    // Late binding for both:
+    cout << "p1->speak() = " << p1->speak() << endl;
+    cout << "p2.speak() = " << p2.speak() << endl;
+    // Early binding (probably):
+
+    // cout << "p3.speak() = " << p3.speak() << endl;
+
+    return 0;
+} ///:~
