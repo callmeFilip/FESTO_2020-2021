2456dcdf2b93d8873906527903026107d8b76f6b
callmeFilip <filip_andonow@abv.bg>
2021-03-04 14:16:40 +0200

    #309

--- Chapter_15/Task_15/Task_15.cpp
+++ Chapter_15/Task_15/Task_15.cpp
@@ -5,16 +5,31 @@ class Base
 private:
 public:
     Base();
-    virtual void func1() { std::cout << __PRETTY_FUNCTION__ << std::endl; }
+    virtual void function() { std::cout << __PRETTY_FUNCTION__ << std::endl; }
+};
+
+class Derived : public Base
+{
+public:
+    Derived() : Base() {}
+    void function()
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+    }
 };
 
 Base::Base()
 {
-    func1();
+    std::cout << "In Base::Base().." << std::endl;
+    function();
 }
 
 int main()
 {
+    Derived d;
+    Base &b = d;
+    std::cout << "Calling function after cast.." << std::endl;
+    b.function();
 
     return 0;
 }
