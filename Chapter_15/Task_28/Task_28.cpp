98e22317b78d427b4492d6ba960337562183c662
Filip Andonov <filip_andonow@abv.bg>
2021-03-05 15:18:11 +0200

    #322

---
+++ Chapter_15/Task_28/Task_28.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+class X
+{
+private:
+public:
+    virtual void vf()
+    {
+        std::cout << "X::vf()" << std::endl;
+    }
+};
+
+class Y : public X
+{
+private:
+public:
+};
+
+int main()
+{
+    Y y;
+    /**
+     * I am not really sure about this one.
+     * I think that constructors and destructors
+     * are generated and called from the derived class
+    */
+    return 0;
+}
